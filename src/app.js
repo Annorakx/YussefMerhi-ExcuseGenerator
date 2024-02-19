1c7a905b1bc30e56d0b3c84d5b3d0f3b50416ed5
Yussef Merhi <101605162+Annorakx@users.noreply.github.com>
2024-02-19 20:09:33 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
+
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let pronoun = ["A", "The"];
+
+  let subject = ["jogger", "racoon", "dog", "driver", "comedian", "pincone"];
+
+  let action = ["took my", "threw my", "yelled at my", "stole my", "bit my"];
+
+  let possetion = ["homework", "toe", "car", "shoe"];
+
+  let where = ["on the street", "in my house", "in my driveway"];
+
+  let proIndx = Math.floor(Math.random() * pronoun.length);
+  let subjIndx = Math.floor(Math.random() * subject.length);
+  let actIndx = Math.floor(Math.random() * action.length);
+  let posIndx = Math.floor(Math.random() * possetion.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+
+  return `${pronoun[proIndx]} ${subject[subjIndx]} ${action[actIndx]} ${possetion[posIndx]} ${where[whereIndex]}`;
 };
